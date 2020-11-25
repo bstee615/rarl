1fa25f13c1d60d8f785475739701503ca3155b19
bstee615 <benjaminjsteenhoek@gmail.com>
2020-11-25 01:05:33 -0600

    Clean up base env

--- env.py
+++ env.py
@@ -1,47 +1,46 @@
 import gym
-import stable_baselines3
 from gym.envs.classic_control.acrobot import *
 
 
-class BaseEnv(AcrobotEnv):
-    def __init__(self, main_agent, adv_agent, friction, blahblah):
-        super(BaseEnv, self).__init__()
-        self.blahblah = blahblah
-        self.friction = friction
-        self.adv_agent = adv_agent
-        self.main_agent = main_agent
+class BaseAcrobotEnv(AcrobotEnv):
+    """Base env for Acrobot task which bridges between main and adversarial agent"""
 
-    def special_step(self, main_action, adv_action):
-        # s = self.state
-        # torque = self.AVAIL_TORQUE[main_action]+adv_action
-        #
-        # # Add noise to the force action
-        # if self.torque_noise_max > 0:
-        #     torque += self.np_random.uniform(-self.torque_noise_max, self.torque_noise_max)
-        #
-        # # Now, augment the state with our force action so it can be passed to
-        # # _dsdt
-        # s_augmented = np.append(s, torque)
-        #
-        # ns = rk4(self._dsdt, s_augmented, [0, self.dt])
-        # # only care about final timestep of integration returned by integrator
-        # ns = ns[-1]
-        # ns = ns[:4]  # omit action
-        # # ODEINT IS TOO SLOW!
-        # # ns_continuous = integrate.odeint(self._dsdt, self.s_continuous, [0, self.dt])
-        # # self.s_continuous = ns_continuous[-1] # We only care about the state
-        # # at the ''final timestep'', self.dt
-        #
-        # ns[0] = wrap(ns[0], -pi, pi)
-        # ns[1] = wrap(ns[1], -pi, pi)
-        # ns[2] = bound(ns[2], -self.MAX_VEL_1, self.MAX_VEL_1)
-        # ns[3] = bound(ns[3], -self.MAX_VEL_2, self.MAX_VEL_2)
-        # self.state = ns
-        # terminal = self._terminal()
-        # reward = -1. if not terminal else 0.
-        # return (self._get_ob(), reward, terminal, {})
-        self.AVAIL_TORQUE = adv_action
-        return self.step(main_action)
+    def __init__(self):
+        super(BaseAcrobotEnv, self).__init__()
+        self.adv_agent = None
+        self.main_agent = None
+
+    def step_two_actions(self, main_action, adv_action):
+        """Step the environment with an action from both the main agent and the adversarial agent."""
+
+        s = self.state
+        torque = self.AVAIL_TORQUE[main_action]  # TODO add torque from adversarial action
+
+        # Add noise to the force action
+        if self.torque_noise_max > 0:
+            torque += self.np_random.uniform(-self.torque_noise_max, self.torque_noise_max)
+
+        # Now, augment the state with our force action so it can be passed to
+        # _dsdt
+        s_augmented = np.append(s, torque)
+
+        ns = rk4(self._dsdt, s_augmented, [0, self.dt])
+        # only care about final timestep of integration returned by integrator
+        ns = ns[-1]
+        ns = ns[:4]  # omit action
+        # ODEINT IS TOO SLOW!
+        # ns_continuous = integrate.odeint(self._dsdt, self.s_continuous, [0, self.dt])
+        # self.s_continuous = ns_continuous[-1] # We only care about the state
+        # at the ''final timestep'', self.dt
+
+        ns[0] = wrap(ns[0], -pi, pi)
+        ns[1] = wrap(ns[1], -pi, pi)
+        ns[2] = bound(ns[2], -self.MAX_VEL_1, self.MAX_VEL_1)
+        ns[3] = bound(ns[3], -self.MAX_VEL_2, self.MAX_VEL_2)
+        self.state = ns
+        terminal = self._terminal()
+        reward = -1. if not terminal else 0.
+        return (self._get_ob(), reward, terminal, {})
 
     def get_ob(self):
         return self._get_ob()
