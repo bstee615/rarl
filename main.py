6db699b86a1b1f6c4e93e23268ba8e11fd148160
bstee615 <benjaminjsteenhoek@gmail.com>
2020-11-25 10:21:07 -0600

    Add rarl environment code to main.py (adv agent currently does nothing)

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-import gym
 from stable_baselines3 import PPO
 
-env = gym.make('Acrobot-v1')
+# Set up environments
+from gym_rarl.envs.adv_acrobot import AdversarialAcrobotEnvWrapper
+from gym_rarl.envs.rarl_env import MainRarlEnv, AdversarialRarlEnv
 
-model = PPO("MlpPolicy", env, verbose=1)
-# Train the agent
-model.learn(total_timesteps=1000000)
-model.save("ppo-acrobot-bleh")
+base_env = AdversarialAcrobotEnvWrapper()
+main_env = MainRarlEnv(base_env)
+adv_env = AdversarialRarlEnv(base_env)
 
-model.load("ppo-acrobot-bleh")
+# Set up agents
+main_agent = PPO("MlpPolicy", main_env, verbose=1)
+adv_agent = PPO("MlpPolicy", adv_env, verbose=1)
+
+# Link agents
+base_env.link_agents(main_agent, adv_agent)
+
+# Main agent tries to act
+obs = main_env.reset()
+a, _ = main_agent.predict(obs)
+main_env.step(a)
+
+# Adversarial agent tries to act
+obs = adv_env.reset()
+a, _ = adv_agent.predict(obs)
+adv_env.step(a)
+
+env = main_env
+model = main_agent
+
+# %% Train the agent
+model.learn(total_timesteps=10000)
+model.save("models/ppo-acrobot-bleh")
+
+# %%
+model.load("models/ppo-acrobot-bleh")
 obs = env.reset()
 for ts in range(5000):
     env.render()
