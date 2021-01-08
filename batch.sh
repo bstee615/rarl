0c7a4b19e3217b6bf79c1a3d769005f3ea6c7103
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-08 15:53:20 -0600

    Update training scripts

--- batch.sh
+++ batch.sh
@@ -1,1 +1,1 @@
-sbatch -t1-00:00:00 --gres=gpu:1 --cpus-per-task=24 --mem=64G --output=logs/job.%J.out --error=logs/job.%J.err --job-name="$1" --nodes=1 $1
+sbatch -t1-00:00:00 --cpus-per-task=8 --mem=64G --output=logs/job.%J.out --error=logs/job.%J.err --job-name="$1" --nodes=1 $1
--- control.sh
+++ control.sh
@@ -3,4 +3,4 @@
 module load miniconda3
 source activate /work/LAS/weile-lab/benjis/weile-lab/envs/rarl
 cd /work/LAS/weile-lab/benjis/weile-lab/rarl
-python3 main.py --control --name=original-lerrel
+python3 main.py --control --name=original-lerrel-big
