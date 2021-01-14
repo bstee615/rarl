5d47afe39d579f35a4ecef7a3fa031e04c2b3b16
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-14 03:06:28 -0600

    Fix rarl training

--- batch.sh
+++ batch.sh
@@ -1,1 +1,1 @@
-sbatch -t1-00:00:00 --cpus-per-task=8 --mem=64G --output=logs/job.%J.out --error=logs/job.%J.err --job-name="$1" --nodes=1 $1
+sbatch -t1-00:00:00 --cpus-per-task=8 --mem=64G --output=logs/job.%J.out --error=logs/job.%J.err --job-name="$1" --nodes=1 $@
--- run-rarl-control.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load miniconda3
-source activate /work/LAS/weile-lab/benjis/weile-lab/envs/rarl
-cd /work/LAS/weile-lab/benjis/weile-lab/rarl
-python3 main.py --control --name=original-lerrel
