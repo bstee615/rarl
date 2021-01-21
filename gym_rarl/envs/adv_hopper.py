85ca84862c24518694db9671598ab18b12fd5c5b
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-21 16:07:50 -0600

    Copy adv_hopper.py from Bullet library

---
+++ gym_rarl/envs/adv_hopper.py
@@ -0,0 +1,122 @@
+import gym
+from gym.envs.classic_control.acrobot import *
+from pybullet_envs.gym_locomotion_envs import HopperBulletEnv
+
+from gym_rarl.envs.adv_env import BaseAdversarialEnv, get_link_by_name
+
+
+class AdversarialHopperEnv(BaseAdversarialEnv, HopperBulletEnv):
+    """
+    Wraps Bullet env and allows two actors to act in each step.
+    """
+
+    @property
+    def adv_action_space(self):
+        bounds_mag = np.ones([2])
+        return gym.spaces.Box(-bounds_mag, bounds_mag)
+
+    def __init__(self, adv_percentage, **kwargs):
+        HopperBulletEnv.__init__(self, **kwargs)
+
+        self.adv_force_mag = 1.0 * adv_percentage  # TODO tune this parameter
+        self.foot_link_i = None
+
+    def get_ob(self):
+        return self.robot.calc_state()
+
+    def step_two_agents(self, action, adv_action):
+        """
+        Copied from pybullet (gym==0.17.3, pybullet==3.0.7)
+        """
+        if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
+            self.robot.apply_action(action)  # protagonist action
+            self.apply_adv_action(adv_action)  # antagonist action
+            self.scene.global_step()
+
+        state = self.robot.calc_state()  # also calculates self.joints_at_limit
+
+        self._alive = float(
+            self.robot.alive_bonus(
+                state[0] + self.robot.initial_z,
+                self.robot.body_rpy[1]))  # state[0] is body height above ground, body_rpy[1] is pitch
+        done = self._isDone()
+        if not np.isfinite(state).all():
+            print("~INF~", state)
+            done = True
+
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
+        progress = float(self.potential - potential_old)
+
+        feet_collision_cost = 0.0
+        for i, f in enumerate(
+                self.robot.feet
+        ):  # TODO: Maybe calculating feet contacts could be done within the robot code
+            contact_ids = set((x[2], x[4]) for x in f.contact_list())
+            # print("CONTACT OF '%d' WITH %d" % (contact_ids, ",".join(contact_names)) )
+            if (self.ground_ids & contact_ids):
+                # see Issue 63: https://github.com/openai/roboschool/issues/63
+                # feet_collision_cost += self.foot_collision_cost
+                self.robot.feet_contact[i] = 1.0
+            else:
+                self.robot.feet_contact[i] = 0.0
+
+        electricity_cost = self.electricity_cost * float(np.abs(action * self.robot.joint_speeds).mean(
+        ))  # let's assume we have DC motor with controller, and reverse current braking
+        electricity_cost += self.stall_torque_cost * float(np.square(action).mean())
+
+        joints_at_limit_cost = float(self.joints_at_limit_cost * self.robot.joints_at_limit)
+        debugmode = 0
+        if (debugmode):
+            print("alive=")
+            print(self._alive)
+            print("progress")
+            print(progress)
+            print("electricity_cost")
+            print(electricity_cost)
+            print("joints_at_limit_cost")
+            print(joints_at_limit_cost)
+            print("feet_collision_cost")
+            print(feet_collision_cost)
+
+        self.rewards = [
+            self._alive, progress, electricity_cost, joints_at_limit_cost, feet_collision_cost
+        ]
+        if (debugmode):
+            print("rewards=")
+            print(self.rewards)
+            print("sum rewards")
+            print(sum(self.rewards))
+        self.HUD(state, action, done)
+        self.reward += sum(self.rewards)
+
+        return state, sum(self.rewards), bool(done), {}
+
+    def apply_adv_action(self, adv_action):
+        p = self.robot._p
+        body_i = self.robot.robot_body.bodies[0]
+        foot_link_i = get_link_by_name(p, body_i, 'foot')
+        p.applyExternalForce(
+            body_i, foot_link_i,
+            forceObj=(
+                adv_action[0] * self.adv_force_mag,
+                0.0,  # y = 0
+                adv_action[1] * self.adv_force_mag,
+            ),
+            posObj=(0.0, 0.0, 0.0),
+            flags=p.WORLD_FRAME)
+
+
+def main():
+    env = AdversarialHopperEnv(render=False, adv_percentage=1.0)
+    env.reset()
+    for _ in range(1000):
+        env.render()
+        # sleep(1 / 30)
+        env.step_two_agents(env.action_space.sample(), env.adv_action_space.sample())
+        # env.step(0)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
